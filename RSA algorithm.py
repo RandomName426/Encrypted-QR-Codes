352d61379ef46f69357f81518a312f6a8547be72
RandomName426 <joshualaluz2008@gmail.com>
2025-01-21 14:38:25 +0000

    Finished rsa OAEP encoding

--- RSA algorithm.py
+++ RSA algorithm.py
@@ -4,7 +4,7 @@ import hashlib
 
 message = input("Imput Message\n>>> ")
 dataArr = []
-for i in message.split():
+for i in list(message):
     dataArr.append(ord(i))
 
 def seedGen(size):
@@ -22,12 +22,13 @@ def oaepPadding(message, hashSize, keySize):
     label = b""
     hashLabel = sha256(label)
     hashMessage = sha256(message)
-    seedXor = bytes([seed ^ hashLabel[i % len(hashLabel)] for i in range (hashSize)])
+    seedXor = bytes([seed[i] ^ hashLabel[i % len(hashLabel)] for i in range (hashSize)])
     maskedMessage = bytes([hashMessage[i] ^ seedXor[i % len(seedXor)] for i in range (hashSize)])
     paddedMessage = seed + maskedMessage
-    if paddedMessage > keySize:
+    paddedMessageInt = int.from_bytes(paddedMessage)
+    if paddedMessageInt.bit_length() > keySize*8:
         raise ValueError("Message is too long for OAEP padding with the key size used")
-    return paddedMessage 
+    return paddedMessageInt
 
 def rsaEncoding(message,publicKey):
     return pow(message,publicKey[0],publicKey[1])
@@ -43,25 +44,24 @@ def rsaOaepEncryption(plainTxtArr, publicKey):
     encryptedTxtData = []
     for chunk in messageChunks:
         chunkBytes = bytes(chunk)
-        paddedMessage = oaepPadding(chunkBytes,sha256HashSize,rsaKeySize)
-        messageInt = int.from_bytes(paddedMessage,"big")
+        messageInt = oaepPadding(chunkBytes,sha256HashSize,rsaKeySize)
         encryptedChunk = rsaEncoding(messageInt,publicKey)
         encryptedTxtData.append(encryptedChunk)
-    return "".join(encryptedTxtData)
+    return encryptedChunk
 
 def rsaOaepDecryption(encryptedTxt,privateKey,hashSize,label=b""):
     decryptedMessage = rsaDecrypytion(int(encryptedTxt),privateKey)
-    paddedMessage = decryptedMessage.to_bytes(decryptedMessage.bit_length()+7//8,"big")
-    seed = paddedMessage[:hashSize]
-    maskedMessage = paddedMessage[hashSize:]
+    paddedMessage = decryptedMessage.to_bytes((decryptedMessage.bit_length() +7) // 8,"big")
+    seed = paddedMessage[len(paddedMessage) - (2*hashSize):len(paddedMessage) - hashSize]
+    maskedMessage = paddedMessage[len(paddedMessage) - hashSize:]
     hashLabel = sha256(label)
-    recoverdSeed = bytes([seed[i] ^ hashLabel[i % len(hashLabel)]] for i in range(hashSize))
-    recoverdMessage = bytes([recoverdMessage[i] ^ recoverdSeed[i % len(recoverdSeed)] for i in range(hashSize)])
-    return recoverdMessage
+    recoverdSeed = bytes(seed[i] ^ hashLabel[i % len(hashLabel)] for i in range(hashSize))
+    recoverdMessage = bytes([maskedMessage[i] ^ recoverdSeed[i % len(recoverdSeed)] for i in range(hashSize)])
+    ''.join([chr(byte) for byte in recoverdMessage if byte != 0])
 
 publicKey,privateKey = KeyGenerator.main()
 x= rsaOaepEncryption(dataArr,publicKey)
-y= rsaOaepDecryption(x.split(),privateKey)
-initalData = " ".join(dataArr)
+y= rsaOaepDecryption(int(x),privateKey,32)
+initalData = " ".join(str(dataArr))
 print(f"Initial Data: {initalData}")
 print(f"\n\nPublic Key: {publicKey}\n\nEncrypted Data: {x}\n\nPrivate Key: {privateKey}\n\nDecrypted Data (should be identical to initial data): {y}")
